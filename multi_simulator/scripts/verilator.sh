c1a4f2125124f559e2c673e92820186ca79d399e
sergioasn <sergioasn@sergioasn>
2020-04-29 16:22:22 +0200

    .

--- multi_simulator/scripts/verilator.sh
+++ multi_simulator/scripts/verilator.sh
@@ -1,11 +1,13 @@
-apt install -y verilator
+# apt-get install -y verilator
 
-# en su defecto para descargar un tags
-# git clone https://git.veripool.org/git/verilator
+#
+# # en su defecto para descargar un tags
+git clone https://git.veripool.org/git/verilator
 # cd verilator
-# git pull
-# git checkout tags/<tag>
-# autoconf        # Create ./configure script
-# ./configure
-# make
-# sudo make install
+cd verilator
+git pull
+git checkout tags/v4.032
+autoconf        # Create ./configure script
+./configure
+make
+make install
